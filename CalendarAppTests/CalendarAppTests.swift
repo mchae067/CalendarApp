f0b9f4b9f3b22fb8025d7fbb5df21ef72e312af9
Mark Chae <MarkChae@204-15-29-143.restechservices.net>
2015-10-19 21:06:46 -0500

    Initial Commit

---
+++ CalendarAppTests/CalendarAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalendarAppTests.swift
+//  CalendarAppTests
+//
+//  Created by Mark Chae on 10/19/15.
+//  Copyright (c) 2015 Mark Chae. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CalendarAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
